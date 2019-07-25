605b6718210db0133eba34984c5070f38be34030
nommy <win.back.0@gmail.com>
2019-07-25 10:30:00 +0800

    メッセージ

--- check.php
+++ check.php
@@ -1,12 +1,27 @@
 <?php
+    
+
     $nickname = $_POST['nickname'];
-    echo $nickname;
-
     $email = $_POST['email'];
-    echo $email;
-
     $content = $_POST['content'];
-    echo $content;
+
+    if ($nickname == '') {
+        $nickname_result = 'ニックネームが入力されていません。';
+    } else {
+        $nickname_result = 'ようこそ、' . $nickname .'様';
+    }
+    
+    if ($email == '') {
+        $email_result = 'メールアドレスが入力されていません。';
+    } else {
+        $email_result = 'メールアドレス：' . $email;
+    }
+
+    if ($content == '') {
+        $content_result =  'お問い合わせ内容が入力されていません。';
+    } else {
+        $content_result = 'お問い合わせ内容：' . $content;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -16,5 +31,22 @@
 </head>
 <body>
     <h1>入力内容確認</h1>
+    <p><?php echo $nickname_result; ?></p>
+    <p><?php echo $email_result; ?></p>
+    <p><?php echo $content_result; ?></p>
+    <form method="POST" action="thanks.php">
+        <input type="hidden" name="nickname" value="<?php echo $nickname; ?>">
+        <input type="hidden" name="email" value="<?php echo $email; ?>">
+        <input type="hidden" name="content" value="<?php echo $content; ?>">
+        <input type="button" value="戻る" onclick="history.back()">
+        <?php if ($nickname != '' && $email != '' && $content != ''): ?>
+            <input type="submit" value="OK">
+        <?php endif; ?>
+    </form>
+<?php
+     if ($nickname != '' && $email != '' && $content != '') {
+      }
+
+?>
 </body>
 </html>
